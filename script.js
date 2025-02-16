5282ecf755cedb931648264c473f1141eb523726
truegoals <vapotrader7@gmail.com>
2025-02-16 18:01:00 +0100

    Add files via upload
    Signed-off-by: truegoals <vapotrader7@gmail.com>

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const overlay = document.getElementById('stream-overlay');
+    
+    overlay.addEventListener('click', function() {
+        window.open('https://www.effectiveratecpm.com/jxn3eb514?key=6190bee5dd50481bd84dd51bdb987985', '_blank');
+        overlay.classList.add('hidden');
+    });
+});
